d513c74c9a8dcf1d2e0a40c130f261cb64a68d87
Angiephoenix <angiephoenix93@gmail.com>
2023-11-04 21:02:33 +0100

    experience update

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [" a Project Manager,", " a Scrum Master,", " an Agile Coach."],
+        strings: [" A Project Manager,", " A Scrum Master,", " An Agile Coach."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [" a Project Manager,", " a Scrum Master,", " an Agile Coach."],
+        strings: [" A Project Manager,", " A Scrum Master,", " An Agile Coach."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
